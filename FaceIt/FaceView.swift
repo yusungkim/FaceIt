54c6810bda74baa47b8d0600c2b7befd848532f0
KimYusung <yusungkim@me.com>
2017-09-05 16:22:53 +0900

    Added empty view

---
+++ FaceIt/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by KimYusung on 9/5/17.
+//  Copyright © 2017 yusungkim. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
