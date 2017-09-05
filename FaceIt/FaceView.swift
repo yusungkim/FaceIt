f0c86baf32f219588ec70d33d778f28700ce2b4c
KimYusung <yusungkim@me.com>
2017-09-05 16:40:07 +0900

    Skull Draw

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 class FaceView: UIView {
+    
+    var scale: CGFloat = 0.9
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        let skullRadius = min(bounds.size.width, bounds.size.height) / 2 * scale
+        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)//convert(center, from: superview)
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: CGFloat.pi * 2.0, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.blue.set()
+        path.stroke()
     }
-    */
-
 }
