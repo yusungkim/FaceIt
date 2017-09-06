b8dccd495ab077525319fdb99f619a5ce22da703
KimYusung <yusungkim@me.com>
2017-09-06 11:33:35 +0900

    Model

--- FaceIt/ViewController.swift
+++ FaceIt/ViewController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            updateUI()
+        }
+    }
 
+    var expression = FacialExpression(eyes: .open, mouth: .grin) {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    private func updateUI()
+    {
+        switch expression.eyes {
+        case .open:
+            faceView?.eyesOpen = true
+        case .closed:
+            faceView?.eyesOpen = false
+        case .squinting:
+            faceView?.eyesOpen = false
+        }
+        faceView?.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+    }
+    
+    private let mouthCurvatures = [FacialExpression.Mouth.grin:0.5,
+                                   .frown: -1.0,
+                                   .smile:1.0,
+                                   .neutral:0.0,
+                                   .smirk:-0.5]
 }
 
